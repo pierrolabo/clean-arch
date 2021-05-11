bb756ed017fab49547dcaed1dab21d1f8998f01b
pierrolabo <pierrolabo@gmail.com>
2021-05-11 14:59:44 +0200

    add productloader factory

--- src/app/product/configuration/productDI.factory.ts
+++ src/app/product/configuration/productDI.factory.ts
@@ -1,14 +1,14 @@
-import { InMemoryProductLoader } from "../adapters/secondaries/inmemory/inMemoryProduct.loader";
 import { LocalJSONProductLoader } from "../adapters/secondaries/local/localJSONProductLoader";
 import { RESTProductLoader } from "../adapters/secondaries/real/RESTProductLoader";
 import { ProductLoader } from "../domain/loaders/productLoader";
-import { ProductBuilder } from "../usecases/product.builder";
 
 export class ProductDIFactory {
     static productLoader(http: any) : ProductLoader {
         switch(process.env.SOURCE) {
             case "fake":
                 return new LocalJSONProductLoader()
+            case "rest-fake":
+                return new RESTProductLoader(http)    
             default:
                 return new LocalJSONProductLoader()
         }
